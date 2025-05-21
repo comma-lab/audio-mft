14a250ff0a83784c7d50ba49245ad3a392ea5263
charis saitis <charis.saitis@gmail.com>
2025-05-21 14:51:17 +0100

    Add files via upload

--- audio_feature_extraction_scripts/utils_construct_dataset.py
+++ audio_feature_extraction_scripts/utils_construct_dataset.py
@@ -1,7 +1,7 @@
 from audiofeatureextractor import DatasetConstructor
 
 # Extract features and save dataset in dictionary format
-dataset_dict = DatasetConstructor.extract_from_folder("/Users/benjaminheyderman/Documents/QM_Final_Project_Research/Track-Preview-Scrape/final/MoralAnnotations/en_200")
+dataset_dict = DatasetConstructor.extract_from_folder("../datasets/en_200")
 
 # Convert to dataframe
 dataset_df = DatasetConstructor.dict2df(dataset_dict)
